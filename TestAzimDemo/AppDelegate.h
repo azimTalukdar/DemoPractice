342abd9d83a8b02b000d384d4aabdde6e26f9828
GENEXT-PC <sandip.khedkar@wwindia.com>
2018-10-09 13:48:00 +0530

    Initial Commit

---
+++ TestAzimDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAzimDemo
+//
+//  Created by GENEXT-PC on 09/10/18.
+//  Copyright © 2018 Azim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
